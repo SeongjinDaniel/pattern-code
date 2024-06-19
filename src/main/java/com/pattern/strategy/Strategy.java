b053b7fffd718567b8480d13f5ca4fdb0c50e4f2
seongjinOliver <yoosngj@gmail.com>
2024-06-20 08:57:05 +0900

    전략패턴

---
+++ src/main/java/com/pattern/strategy/Strategy.java
@@ -0,0 +1,100 @@
+package com.pattern.strategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+interface PaymentStrategy {
+    void pay(int amount);
+}
+
+class KAKAOCardStrategy implements PaymentStrategy {
+    private String name;
+    private String cardNumber;
+    private String cvv;
+    private String dateOfExpiry;
+
+    public KAKAOCardStrategy(String name, String cardNumber, String cvv, String dateOfExpiry) {
+        this.name = name;
+        this.cardNumber = cardNumber;
+        this.cvv = cvv;
+        this.dateOfExpiry = dateOfExpiry;
+    }
+
+    @Override
+    public void pay(int amount) {
+        System.out.println(amount + " paid using KAKAOCard.");
+    }
+}
+
+class LUNACardStrategy implements PaymentStrategy {
+    private String emailId;
+    private String password;
+
+    public LUNACardStrategy(String emailId, String password) {
+        this.emailId = emailId;
+        this.password = password;
+    }
+
+    @Override
+    public void pay(int amount) {
+        System.out.println(amount + " paid using LUNACard.");
+    }
+}
+
+class Item {
+    private String name;
+    private int price;
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
+
+class ShoppingCart {
+    List<Item> items;
+
+    public ShoppingCart() {
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(Item item) {
+        this.items.add(item);
+    }
+
+    public void removeItem(Item item) {
+        this.items.remove(item);
+    }
+
+    public int calculateTotal() {
+        return items.stream().mapToInt(Item::getPrice).sum();
+    }
+
+    public void pay(PaymentStrategy paymentStrategy) {
+        int amount = calculateTotal();
+        paymentStrategy.pay(amount);
+    }
+}
+
+public class Strategy {
+    public static void main(String[] args) {
+        ShoppingCart cart = new ShoppingCart();
+        Item item1 = new Item("1234", 10);
+        Item item2 = new Item("5678", 40);
+
+        cart.addItem(item1);
+        cart.addItem(item2);
+
+        // pay by LUNACard
+        cart.pay(new LUNACardStrategy("test@test.com", "password"));
+        // pay by KAKAOCard
+        cart.pay(new KAKAOCardStrategy("Yoo Seongjin", "1234567890123456", "123", "12/15"));
+    }
+}
